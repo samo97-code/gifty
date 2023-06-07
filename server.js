181473ccb55f74fb20771364832cb4955f634205
Samvel <samo97danielyan@gmail.com>
2023-06-07 07:49:39 +0400

    aa

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const jsonServer = require("json-server")
 const server = jsonServer.create()
-const router = jsonServer.router("https://gifty-json-server.vercel.app/db")
+const router = jsonServer.router("https://gifty-json-server.vercel.app/db.json")
 const middlewares = jsonServer.defaults({
     static: './build'
 })
 
-const fs = require('fs')
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
-const router = jsonServer.router(db)
 
 const port = process.env.Port || 8081
 server.use(middlewares)
